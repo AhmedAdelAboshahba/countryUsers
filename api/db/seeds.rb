4b4cb64da44d07e6c6b73d8b083e1056566c92c1
Ahmed Adel <ahmed.adel@pharos-solutions.de>
2021-09-16 20:16:50 +0200

    printing time taken to seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -6,5 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'start reading file ....'
+t1 = Time.now
 users_attributes = CsvUtils.read_users('test_file')
-User.insert_all(users_attributes)
+t2 = Time.now
+puts "File read in #{t2 - t1} seconds"
+
+puts 'start inserting data ...'
+t1 = Time.now
+User.transaction do
+  User.insert_all(users_attributes)
+end
+t2 = Time.now
+puts "Data inserted in #{t2 - t1} seconds"
